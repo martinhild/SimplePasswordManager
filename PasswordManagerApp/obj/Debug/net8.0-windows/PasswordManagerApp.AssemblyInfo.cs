dc354c34d5426e17aa44cc0ddaaf1092fbec5327
martinhild <martin@nimohi.de>
2025-07-02 11:52:38 +0200

    new mockup

--- PasswordManagerApp/obj/Debug/net8.0-windows/PasswordManagerApp.AssemblyInfo.cs
+++ PasswordManagerApp/obj/Debug/net8.0-windows/PasswordManagerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManagerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d4b4205d51eed9056a8ad2300a79576a3491e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManagerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManagerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
